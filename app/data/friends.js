501d4e3ef1e1a9b8033e328e8109e2aa37216c94
Tavleen Singh <tavleensingh@tavleens-air.attlocal.net>
2017-02-11 22:32:37 -0800

    worked on some logic in survey.html file and set up routing and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+
+//will hold all of the data for new members
+
+var friends = [
+{"name":"Jackie",
+  "photo":"http://rack.1.mshcdn.com/media/ZgkyMDE2LzAxLzI4LzViL3dvcnJpZWRjYXQuNGUyM2EuanBnCnAJdGh1bWIJMTIwMHg2MjcjCmUJanBn/40fe1d67/42c/worried-cat.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1]},
+
+{"name":"Tom",
+  "photo":"https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg",
+  "scores":[
+     5,
+     1,
+     2,
+     4,
+     2,
+     1,
+     5,
+     5,
+     2,
+     1]},
+{"name":"Kim",
+   "photo":"http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-17.jpg",
+   "scores":[
+      1,
+      1,
+      2,
+      1,
+      2,
+      1,
+      5,
+      3,
+      1,
+      1]}
+];
+
+// This makes it accessible to other files using require. 
+module.exports = friends; 
+
+
+
